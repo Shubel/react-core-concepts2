43d39629704f6458b0001b24b409c15943b89e0a
Sunny <shubelislam.diu@gmail.com>
2021-09-20 13:07:36 +0600

    use Effect added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
 
 function Counter() {
   const [count, setCount] = useState(10);
@@ -22,6 +24,36 @@ function Counter() {
       <h2>Count: {count}</h2>
       <button onClick={handleIncrese}>Increase:</button>
       <button onClick={handleDecrese}>Decrease:</button>
+    </div>
+  );
+}
+
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  return (
+    <div>
+      <h3>External Users</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  );
+}
+
+
+function User(props) {
+  return (
+    <div className="product">
+      <h2>name: {props.name}</h2>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
